4c6dfc592afff19b9266f05407cb0cb593c82331
LETTER2418 <165466595+LETTER2418@users.noreply.github.com>
2024-11-28 23:16:21 +0800

    Update SocketManager.h

--- SocketManager.h
+++ SocketManager.h
@@ -5,6 +5,9 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<map>
+#include<queue>
+#include<thread>
 #include "json/json.h" 
 #pragma comment (lib, "ws2_32.lib")
 
@@ -18,14 +21,18 @@ public:
 	bool StartServer();
 	bool ServerAcceptClient();
 	bool ServerSendMsg(SOCKET, Json::Value);
-	Json::Value ServerRecMsg(SOCKET);
-	void ServerRecClientsMsg();
+	Json::Value ServerRecvMsg(SOCKET);
+	void ServerRecvClientsMsg();
+	void ServerAddSendMsgList(SOCKET Client,Json::Value);
+	void ServerProcessSendMsgList();
 
 	void StartClient();//如果StartServer失败就StartCient 
 	void ClientSendMsg(Json::Value);//发送客户端的行为并返回服务端的反馈
+	void ClientAddSendMsgList(Json::Value);
+	void ClientProcessSendMsgList();
 	std::vector<SOCKET> GetClientSockets();
 	SOCKET GetClientSocket();
-	Json::Value ClientRecMsg();//接受消息
+	Json::Value ClientRecvMsg();//接受消息
 
 	void ServerTest();
 	void ClientTest();
@@ -34,14 +41,22 @@ public:
 
 private:
 	WSADATA wsaData;
-	SOCKET ServerSocket, ClientSocket;
-	static const int  SERVER_PORT = 6666;
-	static const int  RecBufferSize = 5E5;//需要合适,否则溢出会"烫烫烫”
-	static const int  SendBufferSize = 5E5;
+	SOCKET ServerSocket= INVALID_SOCKET, ClientSocket=INVALID_SOCKET;
+	static const int  SERVER_PORT = 1234;
+	static const int  RecvBufferSize = 2E5 ;//溢出会"烫烫烫,  
+	static const int  SendBufferSize = 2E5 ;
 	bool error = 0;
-	char RecMsgBuffer[RecBufferSize];
+	char RecvMsgBuffer[RecvBufferSize];
 	char SendMsgBuffer[SendBufferSize];
+	char tmp[SendBufferSize];
 	std::vector<SOCKET>ClientSockets;//存储连接的套接字 
 	fd_set readfds, writefds, exceptfds;
 	timeval timeout;
+	std::map<SOCKET, std::queue<Json::Value>>ServerSendMsgList;//Server send to client
+	std::map<SOCKET, std::queue<Json::Value>>ServerRecvMsgList;
+	std::queue<Json::Value>ClientRecvMsgList;
+	std::queue<Json::Value>ClientSendMsgList;
+	std::string server_str_msg;
+	std::string client_str_msg;
+	std::string str_tmp;
 };
