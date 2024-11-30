c4e4d29ab2103bb501004a660625120bc79608fb
LETTER2418 <165466595+LETTER2418@users.noreply.github.com>
2024-11-30 11:22:15 +0800

    Update SocketManager.h

--- SocketManager.h
+++ SocketManager.h
@@ -1,13 +1,13 @@
 #pragma once
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#include<winsock.h>
+#include<windows.h>
+//#include <ws2tcpip.h>
 #include<assert.h>
 #include<iostream>
 #include<vector>
 #include<string>
-#include<map>
 #include<queue>
-#include<thread>
+//#include<mutex>
 #include "json/json.h" 
 #pragma comment (lib, "ws2_32.lib")
 
@@ -20,19 +20,21 @@ public:
 	~SocketManager();
 	bool StartServer();
 	bool ServerAcceptClient();
-	bool ServerSendMsg(SOCKET, Json::Value);
-	Json::Value ServerRecvMsg(SOCKET);
+	bool ServerSendMsg(SOCKET, const Json::Value&);// 引用传参避免栈溢出
+	Json::Value& ServerRecvMsg(SOCKET);
 	void ServerRecvClientsMsg();
-	void ServerAddSendMsgList(SOCKET Client,Json::Value);
-	void ServerProcessSendMsgList();
+	void ServerAddSendMsgList(SOCKET Client,const Json::Value&);
+	void ServerProcessClientsSendMsgList();
+	void ServerProcessClientSendMsgList(SOCKET );
 
 	void StartClient();//如果StartServer失败就StartCient 
-	void ClientSendMsg(Json::Value);//发送客户端的行为并返回服务端的反馈
-	void ClientAddSendMsgList(Json::Value);
+	void ClientSendMsg(const Json::Value&);//发送客户端的行为并返回服务端的反馈
+	void ClientAddSendMsgList(const Json::Value&);
 	void ClientProcessSendMsgList();
 	std::vector<SOCKET> GetClientSockets();
 	SOCKET GetClientSocket();
-	Json::Value ClientRecvMsg();//接受消息
+	Json::Value& ClientRecvMsg();//接受消息
+	std::map<SOCKET, int>GetClientsState();
 
 	void ServerTest();
 	void ClientTest();
@@ -43,20 +45,24 @@ private:
 	WSADATA wsaData;
 	SOCKET ServerSocket= INVALID_SOCKET, ClientSocket=INVALID_SOCKET;
 	static const int  SERVER_PORT = 1234;
-	static const int  RecvBufferSize = 2E5 ;//溢出会"烫烫烫,  
-	static const int  SendBufferSize = 2E5 ;
-	bool error = 0;
+	static const int  RecvBufferSize = 5E5 ;//溢出会"烫烫烫,  
+	static const int  SendBufferSize = 5E5 ;
+	//bool error = 0;
 	char RecvMsgBuffer[RecvBufferSize];
 	char SendMsgBuffer[SendBufferSize];
 	char tmp[SendBufferSize];
 	std::vector<SOCKET>ClientSockets;//存储连接的套接字 
 	fd_set readfds, writefds, exceptfds;
 	timeval timeout;
-	std::map<SOCKET, std::queue<Json::Value>>ServerSendMsgList;//Server send to client
+	std::map<SOCKET, std::queue<Json::Value>>ServerSendMsgList;
 	std::map<SOCKET, std::queue<Json::Value>>ServerRecvMsgList;
+	std::map<SOCKET, int>ClientsState;
 	std::queue<Json::Value>ClientRecvMsgList;
 	std::queue<Json::Value>ClientSendMsgList;
 	std::string server_str_msg;
 	std::string client_str_msg;
 	std::string str_tmp;
+	Json::Value msg;//防止栈溢出
+	Json::Value json_value;
+	//std::mutex coutMutex;
 };
